9a3fb5c162bae1c19622715fbe2e299c22d94e21
samin01github <81632817+samin01github@users.noreply.github.com>
2021-11-18 01:03:51 +0600

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+//Q10
+
+int* return_an_array(int n, int& size) {
+
+}
+
+int main(){
+	int num = 123;
+	cout<<return_an_arry(123,size);
+
+
+	return 0;
+}
