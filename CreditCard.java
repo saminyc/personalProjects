cb0f46d6d354e5065d4ba3f8df6582359e9f35bf
saminyc <81632817+saminyc@users.noreply.github.com>
2022-08-31 13:24:24 -0400

    CreditCard.java
    This CreditCard class defines credit card objects that model a simplified version of traditional credit cards. They store information about the customer, issuing bank, account identifier, credit limit, and current balance.

--- CreditCard.java
+++ CreditCard.java
@@ -63,5 +63,23 @@ public class CreditCard {
         System.out.println("Limit: " + card.limit);
         System.out.println("Balance: " + card.balance);
     }
+    
+    //main function
+      public static void main(String[] args) {
+                CreditCard[] wallet = new CreditCard[3];
+
+                wallet[0] = new CreditCard("John Bowman", "California Savings",
+                                "5391 0375 9387 5309", 5000);
+                wallet[1] = new CreditCard("John Bowman", "California Federal",
+                                "3485 0399 3395 1954", 3500);
+                wallet[2] = new CreditCard("John Bowman", "California Finance",
+                                "5391 0375 9387 5309", 2500, 300);
+
+                for (CreditCard card : wallet) {
+                        CreditCard.printSummary(card);
+                        System.out.println('\n');
+                }
+
+        }
 
 }
